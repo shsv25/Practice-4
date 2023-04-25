7e3eb16cce50c64a5171d8e3d2211834b09ef053
Sofiia Shevchenko <118025665+shsv25@users.noreply.github.com>
2023-04-26 02:30:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// 12 варіант
+using System;
+
+namespace Project4.ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Введення кількості членів ряду
+            Console.Write("Введiть кiлькiсть членiв ряду: ");
+            int n = int.Parse(Console.ReadLine());
+
+            // Обчислення та виведення суми членів ряду
+            double sum = CalculateSum(n);
+            Console.WriteLine("Сума членiв ряду: {0}", Math.Round(sum, 6));
+
+            // Обчислення та виведення суми перших 10 членів ряду
+            double sumFirstTen = CalculateSumFirstTen();
+            Console.WriteLine("Сума перших 10 членiв ряду: {0}", Math.Round(sumFirstTen, 6));
+
+            Console.ReadKey();
+        }
+
+        // Метод для обчислення суми членів ряду
+        static double CalculateSum(int n)
+        {
+            double sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                double a = Math.Pow(-1, i) * (1 / Math.Pow(i, 2) + Math.Pow(2, i));
+                sum += a;
+            }
+            return sum;
+        }
+
+        // Метод для обчислення суми перших 10 членів ряду
+        static double CalculateSumFirstTen()
+        {
+            double sumFirstTen = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                double a = Math.Pow(-1, i) * (1 / Math.Pow(i, 2) + Math.Pow(2, i));
+                sumFirstTen += a;
+            }
+            return sumFirstTen;
+        }
+    }
+}
