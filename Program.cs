fa9d3a11797355cd78943ccd79a7d4bdbcd22fe1
Sofiia Shevchenko <118025665+shsv25@users.noreply.github.com>
2023-04-26 02:28:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// 1 варіант
+using System;
+
+namespace Project4.ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть початкове значення a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть кiнцеве значення b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Введiть задане число c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            PrintMultiples(a, b, c); // виклик методу для виведення кратних чисел
+        }
+
+        static void PrintMultiples(int a, int b, int c)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                if (i % c == 0) // перевірка на кратність
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+        }
+    }
+}
+
